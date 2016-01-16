518ebae27642c7be96c95ed2251c125bf2049e0a
sam3killa <samuels.shih@gmail.com>
2016-01-15 21:36:12 -0800

    Finished Loop Test

--- BlocExercises/BlocExercises/AwesomeCounter.m
+++ BlocExercises/BlocExercises/AwesomeCounter.m
@@ -12,7 +12,36 @@
 
 - (NSString *) stringWithNumbersBetweenNumber:(NSInteger)number andOtherNumber: (NSInteger)otherNumber {
     /* WORK HERE */
-    return @"";
+    
+    NSString *emptyString = @"";
+    NSMutableString *mutableStringResult = [emptyString mutableCopy];
+    NSString *containerString;
+    
+    // Check to see if number is less than otherNumber.
+    if (number < otherNumber){
+        while(number <= otherNumber){
+            containerString =[NSString stringWithFormat: @"%ld",(long)number];
+            [mutableStringResult appendString:containerString];
+            number++;
+        }
+    }
+    
+    // Check to see if otherNumber is less than number
+    else if (otherNumber < number){
+        while(otherNumber <= number){
+            containerString =[NSString stringWithFormat: @"%ld",(long)otherNumber];
+            [mutableStringResult appendString:containerString];
+            otherNumber++;
+        }
+    }
+    
+    // Check to see if the numbers are equal
+    else if (otherNumber == number){
+        containerString =[NSString stringWithFormat: @"%ld",(long)number];
+        [mutableStringResult appendString:containerString];
+    }
+    
+    return mutableStringResult;
 }
 
 @end
