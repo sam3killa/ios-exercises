04e47eea832440370fdc3a09aacc00c89d2a64cd
sam3killa <samuels.shih@gmail.com>
2016-01-16 13:06:33 -0800

    Finished Dictionary Tests

--- BlocExercises/BlocExercises/StarTrekDictionaries.m
+++ BlocExercises/BlocExercises/StarTrekDictionaries.m
@@ -12,17 +12,41 @@
 
 - (NSString *)favoriteDrinkForStarTrekCharacterDictionary:(NSDictionary *)characterDictionary {
     /* WORK HERE */
-    return @"";
+    
+    // Retrieve the value stored in the "favorite drink" key in the characterDictionary
+    NSString *favoriteDrink = characterDictionary[@"favorite drink"];
+    
+    return favoriteDrink;
 }
 
 - (NSArray *)arrayOfFavoriteDrinksForStarTrekCharacters:(NSArray *)charactersArray {
-    /* WORK HERE */
-    return @[];
+    
+    // Create an NSMutableArray to store the strings
+    NSMutableArray *myArray = [[NSMutableArray alloc] init];
+    
+    // Iterate through the characters array using a for in loop
+    for (NSDictionary *character in charactersArray) {
+        
+        // Retrive the value stored in the "favorite drink" key and append it to the mutable array called myArray
+        NSString *favoriteDrink = character[@"favorite drink"];
+        [myArray addObject:favoriteDrink];
+    }
+    
+    return myArray;
 }
 
 - (NSDictionary *)dictionaryWithQuoteAddedToStarTrekCharacterDictionary:(NSDictionary *)characterDictionary {
-    /* WORK HERE */
-    return @{};
+    
+    // Create an NSMutableDictionary that is a copy of the characterDictionary
+    NSMutableDictionary *addedQuoteDictionary = [characterDictionary mutableCopy];
+    
+    // Create a random quote
+    NSString *quote = @"Some random quote that this person said";
+    
+    // Add the quote to the dictionary
+    [addedQuoteDictionary setObject:quote forKey:@"quote"];
+    
+    return addedQuoteDictionary;
 }
 
 @end
