2ed2edb7c1a55ad5cfd40fda4ff99329cfb3202b
sam3killa <samuels.shih@gmail.com>
2016-01-16 11:47:33 -0800

    Finished Array Tests

--- BlocExercises/BlocExercises/StarTrekArrays.m
+++ BlocExercises/BlocExercises/StarTrekArrays.m
@@ -11,23 +11,58 @@
 @implementation StarTrekArrays
 
 - (NSArray *) arrayOfStarTrekCharactersFromString:(NSString *)characterString {
-    /* WORK HERE */
-    return @[];
+    
+    // Create an NSArray and use the componentsSeparateByString method
+    NSArray *stringToArray = [characterString componentsSeparatedByString:@";"];
+    
+    return stringToArray;
 }
 
 - (NSString *) stringOfStarTrekCharactersFromArray:(NSArray *)characterArray {
-    /* WORK HERE */
-    return @"";
+    
+    // Create an NSString and use the componentsJoinedByString method
+    NSString *arrayToString = [characterArray componentsJoinedByString:@";"];
+    
+    return arrayToString;
 }
 
 - (NSArray *) alphabeticallySortedStarTrekCharactersFromArray:(NSArray *)characterArray {
-    /* WORK HERE */
-    return @[];
+    
+    // Create a Mutable Array using the characterArray and calling the mutableCopy method on it.
+    NSMutableArray *starTrekMutableArray = [characterArray mutableCopy];
+    
+    // Create an NSSortDescriptor to describe how I want to sort the array. Ascending order, and comparing the strings regardless of case.
+    NSSortDescriptor *sortDescriptor = [[NSSortDescriptor alloc]initWithKey:nil ascending:YES selector:@selector(localizedCaseInsensitiveCompare:)];
+    
+    // Call the sortUsingDescriptors method on the starTrekMutableArray to sort the array
+    [starTrekMutableArray sortUsingDescriptors:@[sortDescriptor]];
+    
+    // Return the array
+    return starTrekMutableArray;
 }
 
 - (BOOL) characterArrayContainsWorf:(NSArray *)characterArray {
-    /* WORK HERE */
-    return NO;
+    
+    // Create a mutable copy of the characterArray
+    NSMutableArray *starTrekMutableArray = [characterArray mutableCopy];
+    
+    // Create an NSPredicate to filter out strings that contain the character "Worf"
+    NSPredicate *worfDetected = [NSPredicate predicateWithFormat:@"SELF CONTAINS[c] 'Worf'"];
+    
+    // Filter the starTrekMutableArray to find only strings that contain the string Worf
+    [starTrekMutableArray filterUsingPredicate:worfDetected];
+    
+    // Check to see if the array contains anything because if a string contains "worf" it will be left in the array
+    if (starTrekMutableArray.count == 0 ){
+        
+        return NO;
+        
+    }
+    else {
+ 
+        return YES;
+    
+    }
 }
 
 @end
